b06f98ef7f7e88e0339df6fbfaf28d5c913e8c9d
MarcinParda <marcin98parda@gmail.com>
2024-05-22 22:14:28 +0200

    feat: app icon and name

--- domains/shared/feature-shared-sidebar/src/lib/feature-shared-sidebar.tsx
+++ domains/shared/feature-shared-sidebar/src/lib/feature-shared-sidebar.tsx
@@ -1,62 +1,55 @@
 import Link from 'next/link';
 import {
-  Home,
-  LineChart,
-  Mail,
-  Package,
-  Package2,
-  Settings,
-  ShoppingCart,
+  SettingsIcon,
   PlusIcon,
+  ArrowLeftRightIcon,
+  HomeIcon,
+  NotebookPenIcon,
+  Grid2X2Icon,
+  WalletIcon,
+  PiggyBankIcon,
 } from 'lucide-react';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@ai-budget-app/ui-shared-tooltip';
 import { Button } from '@ai-budget-app/ui-shared-button';
 import { Separator } from '@ai-budget-app/ui-shared-separator';
 
 export function FeatureSharedSidebar() {
   return (
     <aside className="w-48 inset-y-0 left-0 z-10 hidden flex-col border-r bg-background sm:flex">
+      <div className="flex w-full py-5 gap-2 justify-center items-center">
+        <PiggyBankIcon className="w-8 h-8" />
+        <span className="text-xl">AI budget</span>
+      </div>
+      <Separator />
       <nav className="flex flex-col justify-start items-center px-2 sm:py-5">
         <Link className="w-full" href="/dashboard">
           <Button className="w-full justify-start" variant="link">
-            <Home className="mr-2 h-4 w-4" /> Dashboard
+            <HomeIcon className="mr-2 h-4 w-4" /> Dashboard
           </Button>
         </Link>
         <Link className="w-full" href="/dashboard">
           <Button className="w-full justify-start" variant="link">
-            <Home className="mr-2 h-4 w-4" /> Add transactions
+            <ArrowLeftRightIcon className="mr-2 h-4 w-4" /> Add transactions
           </Button>
         </Link>
         <Link className="w-full" href="/dashboard">
           <Button className="w-full justify-start" variant="link">
-            <Home className="mr-2 h-4 w-4" /> Plan transactions
+            <NotebookPenIcon className="mr-2 h-4 w-4" /> Plan transactions
           </Button>
         </Link>
         <Link className="w-full" href="/dashboard">
           <Button className="w-full justify-start" variant="link">
-            <Home className="mr-2 h-4 w-4" /> Categories
+            <Grid2X2Icon className="mr-2 h-4 w-4" /> Categories
           </Button>
         </Link>
         <Link className="w-full" href="/dashboard">
           <Button className="w-full justify-start" variant="link">
-            <Home className="mr-2 h-4 w-4" /> Current assets
+            <WalletIcon className="mr-2 h-4 w-4" /> Current assets
           </Button>
         </Link>
         <Separator />
         <Link className="w-full" href="/favorites">
           <Button className="w-full justify-start" variant="link">
             <PlusIcon className="mr-2 h-4 w-4" /> Add new link
-          </Button>
-        </Link>
-      </nav>
-      <nav className="mt-auto flex flex-col items-center px-2 sm:py-5">
-        <Link className="w-full" href="/setting">
-          <Button className="w-full justify-start" variant="link">
-            <Settings className="mr-2 h-4 w-4" /> Settings
           </Button>
         </Link>
       </nav>
