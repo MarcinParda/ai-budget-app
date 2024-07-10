c31e409b5e1a1c43f93c303c3df00cdaaef3cec0
MarcinParda <marcin98parda@gmail.com>
2024-07-10 14:54:14 +0200

    feat: Update middleware to handle session and redirect for / and /login

--- apps/cockpit/src/middleware.ts
+++ apps/cockpit/src/middleware.ts
@@ -1,17 +1,25 @@
-import { withAuth } from 'next-auth/middleware';
 import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+import type { NextRequest } from 'next/server';
 
-export default withAuth(
-  function middleware() {
-    return NextResponse.next();
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
+export async function middleware(request: NextRequest) {
+  const session = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  const isLoginPage = request.nextUrl.pathname === '/login';
+
+  if (session && isLoginPage) {
+    return NextResponse.redirect(new URL('/', request.url));
   }
-);
+
+  if (!session && !isLoginPage) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ['/((?!login).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
